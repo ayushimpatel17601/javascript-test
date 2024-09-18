394fb48aa744b23b733c1d3cefde3f03ddca2fd2
Ayushi Patel <140682747+ayushimpatel17601@users.noreply.github.com>
2024-09-18 12:18:07 +0000

    oprators done

--- 01_basics/03_conversionOprators.js
+++ 01_basics/03_conversionOprators.js
@@ -27,3 +27,35 @@ let score4 = 33
 let instring = String(score4);
 console.log(instring);
 console.log(typeof instring);
+
+
+
+// ================= oprators ==================
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello";
+let str2 = "world";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("2"+1);
+console.log(2+"1");
+console.log("2"+"1");
+console.log(2+1);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+// link for reading docs
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
