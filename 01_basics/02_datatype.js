265f6c7cbc077598e9ec082cb751157a646aebf0
Ayushi Patel <140682747+ayushimpatel17601@users.noreply.github.com>
2024-09-18 09:15:57 +0000

    datatype done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code newer virson
+
+
+let name = "Ayushi"
+let age = 23 
+let state;
+
+console.log(name)
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => let state value is undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "hello"); // string
+console.log(typeof age); // number
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
