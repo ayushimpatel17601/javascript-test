1c6437d55fb31fbae4b45df742a4f33e2a57091d
Ayushi Patel <140682747+ayushimpatel17601@users.noreply.github.com>
2024-09-23 13:07:40 +0000

    arrays continue

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,6 @@
+const marvel_heros = ["spiderman","thor","ironman"];
+const dc_heros = ["superman","flash","batman"];
+
+marvel_heros.push(dc_heros);
+console.log(marvel_heros);
+console.log(marvel_heros[1][2]);
